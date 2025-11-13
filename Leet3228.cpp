644b5f1599d3763a9d94b0e9596530a4beeb9760
VivekRai-gif <vivekrai2416@gmail.com>
2025-11-13 20:11:39 +0530

    updated

--- Leet3228.cpp
+++ Leet3228.cpp
@@ -1,8 +1,17 @@
 //3228. Maximum Number of Operations to Move Ones to the End
 //13-11-2023
 class Solution {
-public:
-    int maxOperations(string s) {
-        
-    }
+ public:
+  int maxOperations(string s) {
+    int ans = 0;
+    int ones = 0;
+
+    for (int i = 0; i < s.length(); ++i)
+      if (s[i] == '1')
+        ++ones;
+      else if (i + 1 == s.length() || s[i + 1] == '1')
+        ans += ones;
+
+    return ans;
+  }
 };
