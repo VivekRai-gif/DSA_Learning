dcc7197125574ed3a262295bf61a0b6d5b4dedf5
Vivek Rai <153980989+VivekRai-gif@users.noreply.github.com>
2026-04-28 07:14:37 +0530

    solution for Max Consecutive Ones III
    Implement the longestOnes function to find the maximum length of consecutive 1's in a binary array with at most k flips.

---
+++ 1004. Max Consecutive Ones III.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0;
+        int zeroCount = 0;
+        for (int num : nums) {
+           zeroCount += num ^ 1;
+             if (zeroCount > k) {
+                 zeroCount -= nums[left] ^ 1;
+                left++;
+            }
+        }
+    return nums.size() - left;
+    }
+};
