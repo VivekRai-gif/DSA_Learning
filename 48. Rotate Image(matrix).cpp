c0946c02beba8509b9b2896bb1eb56e101721a62
Vivek Rai <153980989+VivekRai-gif@users.noreply.github.com>
2026-05-04 23:49:21 +0530

    Add rotate function to rotate image matrix
    matrix rotation by transposing and reversing rows.

---
+++ 48. Rotate Image(matrix).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        //transwerse and then reverse all row
+        for (int i=0;i<matrix.size();i++)
+             {for (int j=i+1;j<matrix.size();j++)
+                 {
+            swap(matrix[i][j],matrix[j][i]);
+           
+                    }
+    }
+     for (int i = 0; i < matrix.size(); i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
