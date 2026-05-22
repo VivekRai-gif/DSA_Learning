1fda3963fd688619aa2ec966e519b31ef15d3541
Vivek Rai <153980989+VivekRai-gif@users.noreply.github.com>
2026-05-22 20:26:00 +0530

    Implement linear search in rotated sorted array

---
+++ 33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+int result = -1;
+    int search(vector<int>& nums, int target) {
+        for (int i =0; i< nums.size();i++)
+        {
+            if (nums[i]==target)
+            {
+              return i;
+            }
+        }
+        return -1;
+    }
+};
