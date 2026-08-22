e00a2117c66eb9cbe0625160cf00769fb2790d61
Vivek Rai <153980989+VivekRai-gif@users.noreply.github.com>
2026-08-22 20:51:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (97.17%) - LeetSync

--- 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
@@ -6,12 +6,10 @@ public:
       int product = 1;
     while (n > 0) {
         int digit = n % 10;
-        cout << digit << " ";
         sum += digit;
         product *= digit;
         n = n / 10;
     }
-    int final = sum + product;
-    return (temp%final==0);
+    return (temp% (sum + product)==0);
     }
 };
